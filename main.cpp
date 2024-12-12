f23baefda7385301e2ad3f9c04642e8fab577831
SlowRiot <slowriot@voxelstorm.com>
2024-12-12 13:42:35 +0000

    remove unused using namespace

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 #include "gui/gui_renderer.h"
 #include "render/webgpu_renderer.h"
 
-using namespace std::string_literals;
-
 class game_manager {
   logstorm::manager logger{logstorm::manager::build_with_sink<logstorm::sink::console>()}; // logging system
   render::webgpu_renderer renderer{logger};                                     // WebGPU rendering system
